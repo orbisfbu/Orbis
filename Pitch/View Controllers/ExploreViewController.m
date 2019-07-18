7d4ea21a99d1a52dca25bb47071a7d051337dc9c
Mario Baxter <mariobaxter@fb.com>
2019-07-18 08:46:27 -0700

    Added map functionality to the ExploreViewController

--- Pitch/View Controllers/ExploreViewController.m
+++ Pitch/View Controllers/ExploreViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ExploreViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface ExploreViewController ()
-
+@property (weak, nonatomic) IBOutlet MKMapView *photoMap;
 @end
 
 @implementation ExploreViewController
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.photoMap setRegion:sfRegion animated:false];
+    //self.photoMap.delegate = self;
 }
 
 /*
