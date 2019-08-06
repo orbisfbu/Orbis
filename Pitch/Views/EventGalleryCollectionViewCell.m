a3236bc3c9d50ab805aefba2715c2860e8befdf0
Elizabeth Zietz <ezietz@fb.com>
2019-08-06 11:46:52 -0700

    Added event gallery cell file when merging with Sebastian

---
+++ Pitch/Views/EventGalleryCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  EventGalleryCollectionViewCell.m
+//  Pitch
+//
+//  Created by ezietz on 8/6/19.
+//  Copyright © 2019 PitchFBU. All rights reserved.
+//
+
+#import "EventGalleryCollectionViewCell.h"
+
+@implementation EventGalleryCollectionViewCell
+
+@end
