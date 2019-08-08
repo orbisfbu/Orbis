62a08d3c3f78c6f1a65a479d52d87a189ba599ff
Mario Baxter <mariobaxter@fb.com>
2019-08-07 17:03:35 -0700

    Fixed bug that made the Create Event VC crash when adding too many additional pictures

---
+++ Pitch/Views/CollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.m
+//  Pitch
+//
+//  Created by mariobaxter on 8/7/19.
+//  Copyright © 2019 PitchFBU. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
---
+++ Pitch/Views/MediaCollectionViewCell.h
@@ -0,0 +1,18 @@
+//
+//  MediaCollectionViewCell.h
+//  Pitch
+//
+//  Created by mariobaxter on 8/7/19.
+//  Copyright © 2019 PitchFBU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MediaCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
